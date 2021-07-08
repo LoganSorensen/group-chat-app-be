15973d2d5ffeb9a85c6b07ed6d2133dba647b141
Logan <loganmsorensen@gmail.com>
2021-07-07 20:08:18 -0700

    setup chatMessage and disconnect sockets

--- api/server.js
+++ api/server.js
@@ -1,28 +1,75 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
-const http = require('http')
+const http = require("http");
 const socketio = require("socket.io");
 
 const usersRouter = require("./users/users-router");
 const channelsRouter = require("./channels/channels-router");
 const messagesRouter = require("./messages/messages-router");
 
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require("../utils/users");
+
 const app = express();
-const server = http.createServer(app)
+const server = http.createServer(app);
 const io = socketio(server);
 
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-io.on('connection', (socket) => {
-  console.log('We have a new connection!')
+io.on("connection", (socket) => {
+  console.log("We have a new connection!");
 
-  socket.on('joinRoom', ({username, room}) => {
-    console.log(`${username} has joined ${room}`)
-  })
-})
+  // handles a user joining the room
+  socket.on("joinRoom", ({ username, room }) => {
+    console.log(`${username} has joined ${room}\n`);
+
+    const user = userJoin(socket.id, username, room);
+
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.username} has joined ${user.room}!`,
+      time: Date.now(),
+    });
+
+    socket.join(user.room);
+  });
+
+  // handles a user sending a message
+  socket.on("chatMessage", (message) => {
+    console.log("message recieved");
+    const user = getCurrentUser(socket.id);
+
+    console.log(`sending ${message} to ${user.room}`);
+
+    // io.to(user.room).emit("message", message)
+    io.to(user.room).emit("message", {
+      user: user.username,
+      text: message,
+      time: Date.now(),
+    });
+  });
+
+  // handles a client disconnecting
+  socket.on("disconnect", () => {
+    const user = userLeave(socket.id);
+
+    if (user) {
+      io.to(user.room).emit("message", `${user.username} has left the chat`);
+
+      io.to(user.room).emit("roomUsers", {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      });
+    }
+  });
+});
 
 app.use("/api/users", usersRouter);
 app.use("/api/channels", channelsRouter);
