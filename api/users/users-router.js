668297a64edfa08187bb486c6f9c9da17b52bf1b
Logan <loganmsorensen@gmail.com>
2021-08-14 04:43:30 -0700

    added a route to get users by id

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,10 +1,25 @@
 const router = require("express").Router();
 const Users = require("./users-model");
 
+// Get all users
 router.get("/", (req, res, next) => {
   Users.getAll()
     .then((users) => {
       res.status(200).json(users);
+    })
+    .catch(next);
+});
+
+// Get user by id
+router.get("/:userId", (req, res, next) => {
+  Users.getById(req.params.userId)
+    .then((user) => {
+      if (user) {
+        const { password, ...userObject } = user;
+        res.status(200).json(userObject);
+      } else {
+        res.status(404).json("The user with the specified id does not exist.");
+      }
     })
     .catch(next);
 });
